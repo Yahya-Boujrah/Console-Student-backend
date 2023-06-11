87adb9ac6628d0604dcb7e1607e2c9ca57a3bf19
Rabihi Mouad <rabihimouad@Rabihis-MacBook-Air.local>
2023-06-11 15:00:43 +0100

    fix bug

--- src/main/java/com/consolestudent/model/Note.java
+++ src/main/java/com/consolestudent/model/Note.java
@@ -1,5 +1,6 @@
 package com.consolestudent.model;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
@@ -23,6 +24,8 @@ public class Note {
 
     private String module;
     private Float note;
+
+    @JsonIgnore
     @ManyToOne
     private Result result;
 }

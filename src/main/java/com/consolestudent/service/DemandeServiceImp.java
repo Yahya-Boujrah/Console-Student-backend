403ae1d8e94479bd66d066f57c3b1aca9cf01a9c
Rabihi Mouad <rabihimouad@Rabihis-MacBook-Air.local>
2023-06-11 18:26:50 +0100

    web client test 5

--- src/main/java/com/consolestudent/service/DemandeServiceImp.java
+++ src/main/java/com/consolestudent/service/DemandeServiceImp.java
@@ -12,6 +12,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.reactive.function.BodyInserters;
 import org.springframework.web.reactive.function.client.WebClient;
 import reactor.core.publisher.Mono;
 
@@ -83,7 +84,7 @@ public class DemandeServiceImp implements DemandeService{
                 .post()
                 //.uri("https://ensa-a7-dev-ed.develop.my.salesforce.com/services/apexrest/ServiceRequests/")
                 .header("Authorization", "Bearer " + oauthToken)
-                .body(Mono.just(serviceRequest), ServiceRequest.class)
+                .body(BodyInserters.fromValue(serviceRequest))
                 .retrieve()
                 .bodyToMono(ServiceRequest.class);
 
